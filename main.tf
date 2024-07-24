a88967e62e3ef5a216f75e376a6fa40391731bdb
bermetakub <165640022+bermetakub@users.noreply.github.com>
2024-07-24 23:45:08 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "bermet-example" {
-  ami           = "ami-0427090fd1714168b"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "bermeta-example-instance"
-  }
+#resource "aws_instance" "bermet-example" {
+#  ami           = "ami-0427090fd1714168b"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "bermeta-example-instance"
+#  }
+#}
+resource "aws_key_pair" "bermet_example" {
+  key_name = "bermet-example-key"
 }
