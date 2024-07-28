bfa980a239938e04c57d9486278652f9be3cb18e
bermetakub <alymkulovabk@gmail.com>
2024-07-29 00:12:36 +0600

    move

--- Day5/vars/deployApp.groovy
+++
@@ -1,7 +0,0 @@
-def call(String server, String imageName) {
-    sshagent(['dockerhub-creds']) {
-        sh """
-            ssh -o StrictHostKeyChecking=no ${server} 'docker pull ${imageName} && docker run -d -p 80:8083 ${imageName}'
-        """
-    }
-}
---
+++ vars/deployApp.groovy
@@ -0,0 +1,7 @@
+def call(String server, String imageName) {
+    sshagent(['dockerhub-creds']) {
+        sh """
+            ssh -o StrictHostKeyChecking=no ${server} 'docker pull ${imageName} && docker run -d -p 80:8080 ${imageName}'
+        """
+    }
+}
