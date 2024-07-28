4d1c4384dd991fc0b539c40d2e4077d60de89b9e
bermetakub <165640022+bermetakub@users.noreply.github.com>
2024-07-28 23:57:46 +0600

    Update deployApp.groovy

--- Day5/vars/deployApp.groovy
+++ Day5/vars/deployApp.groovy
@@ -1,7 +1,7 @@
 def call(String server, String imageName) {
     sshagent(['dockerhub-creds']) {
         sh """
-            ssh -o StrictHostKeyChecking=no ${server} 'docker pull ${imageName} && docker run -d -p 8083:80 ${imageName}'
+            ssh -o StrictHostKeyChecking=no ${server} 'docker pull ${imageName} && docker run -d -p 80:8083 ${imageName}'
         """
     }
 }
