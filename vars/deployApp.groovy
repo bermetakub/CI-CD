53af3c9a7012272eee7b7ba2c1be0f6fccd7d19f
bermetakub <alymkulovabk@gmail.com>
2024-07-29 01:21:33 +0600

    update

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,4 @@
-def call(String server, String image) {
-    sshagent(['id_rsa']) {
-        sh "ssh bermet@${172.20.10.10} 'docker pull ${image} && docker run -d -p 8083:80 ${image}'"
-    }
+def call(String deployTarget, String image) {
+    echo "Deploying to target: ${deployTarget} with image: ${image}"
+    sh "ssh bermet@${deployTarget} 'docker pull ${image} && docker run -d -p 8083:80 ${image}'"
 }
-
