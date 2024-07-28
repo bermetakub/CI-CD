e666a883551562fc496030013cd77a3060599379
bermetakub <alymkulovabk@gmail.com>
2024-07-29 01:15:47 +0600

    update

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,6 +1,16 @@
-def call(String branch, String env) {
-    def imageName = "alymkulovabk/simpleapp:${branch}-${env}"
-    sh "docker build -t ${imageName} ."
-    sh "docker push ${imageName}"
+def call(String imageName) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh """
+            docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+            docker build -t ${imageName} .
+            docker push ${imageName}
+        """
+    }
 }
+
+// def call(String branch, String env) {
+//     def imageName = "alymkulovabk/simpleapp:${branch}-${env}"
+//     sh "docker build -t ${imageName} ."
+//     sh "docker push ${imageName}"
+// }
 
